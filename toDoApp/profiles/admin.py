16d35d5204c5cfc82d4d05fd872c9e0d9d210e42
MariaKVelcheva <maria.k.velcheva@gmail.com>
2026-03-04 20:23:44 +0100

    added: admin logic added

--- toDoApp/profiles/admin.py
+++ toDoApp/profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from toDoApp.profiles import models
+from django.contrib.auth import get_user_model
+
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class UserModelAdmin(UserAdmin):
+    pass
